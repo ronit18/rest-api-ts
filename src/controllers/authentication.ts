fea1f1df5a6bef2fc29f99c2ff20dedcee8432a7
ronit18 <ronitgandhi96@gmail.com>
2023-06-04 16:00:42 +0530

    Made user login route.

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -25,7 +25,46 @@ export const register = async (req: express.Request, res: express.Response) => {
 
 		return res.status(201).json(user).end();
 	} catch (error) {
-		console.error('Error in controllers/authentication.ts : ', error);
+		console.error(
+			'Error in controllers/authentication.ts(register) : ',
+			error
+		);
+		return res.sendStatus(400);
+	}
+};
+
+export const login = async (req: express.Request, res: express.Response) => {
+	try {
+		const { email, password } = req.body;
+
+		if (!email || !password) return res.status(400).send('Bad request');
+
+		const user = await getUserByEmail(email).select(
+			'+authentication.salt +authentication.password'
+		);
+
+		if (!user) return res.status(404).send('User not found');
+
+		const expectedHash = authentication(user.authentication.salt, password);
+
+		if (user.authentication.password !== expectedHash)
+			return res.status(401).send('Unauthorized');
+
+		const salt = random();
+		user.authentication.sessionToken = authentication(
+			salt,
+			user._id.toString()
+		);
+		res.cookie('RONIT_AUTH', user.authentication.sessionToken, {
+			domain: 'localhost',
+			path: '/',
+		});
+		return res.status(200).json(user).end();
+	} catch (error) {
+		console.error(
+			'Error in controllers/authentication.ts(login) : ',
+			error
+		);
 		return res.sendStatus(400);
 	}
 };
